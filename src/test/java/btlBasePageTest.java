d5229c2518daee7b8aa537e73914032e9a1257c7
EFRAT-1-cohen <efratco054@gmail.com>
2026-02-09 21:18:49 +0200

    2

--- src/test/java/btlBasePageTest.java
+++ src/test/java/btlBasePageTest.java
@@ -1,44 +1,19 @@
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class btlBasePageTest {
+
     public WebDriver driver;
-    protected static ExtentReports extent;
-    protected static ExtentTest test;
-    protected static ExtentSparkReporter reporter;
-
-    @BeforeAll
-    public static void startTests(){
-        System.out.println("התחלת טסטים");
-        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
-        reporter = new ExtentSparkReporter("./target/ExtentReport/ExtentReport_" + timestamp + ".html");
-        extent = new ExtentReports();
-        extent.attachReporter(reporter);
-    }
-
-    @AfterAll
-    public static void endTests(){
-        System.out.println("סיום טסטים");
-        if (extent != null) {
-            extent.flush();
-        }
-    }
 
     @BeforeEach
     public void loadDriver(){
@@ -74,6 +49,7 @@ public class btlBasePageTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return destination;
     }
 }
